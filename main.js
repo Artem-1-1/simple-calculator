2315c3ec2ce07f17377462a26486fa1ec774e1ef
Artem-1-1 <aleksejchukartem@gmail.com>
2025-10-28 17:41:29 +0300

    Added operate function and const for operand/buttons(excluding numbers and symbols)

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-let num1;
-let num2;
-let operator;
+let firstOperand = '';
+let secondOperand = '';
+let operator = null;
+
+const clearButton = document.getElementById('clearBtn');
+const delButton = document.getElementById('deleteBtn');
+const pointButton = document.getElementById('pointBtn');
+const equalsButton = document.getElementById('equalsBtn');
+const lastOperationScreen = document.getElementById('lastOperationScreen');
+const currentOperationScreen = document.getElementById('currentOPerationScreen');
 
 function add(num1, num2) {
     return num1 + num2;
@@ -16,4 +23,24 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    num1 = Number(num1);
+    num2 = NUmber(num2);
+    if (b === 0) {
+        return alert('Divide by zero')
+    }
+    else if (operator === '+') {
+        return add(num1, num2);
+    }
+    else if (operator === '-') {
+        return subtract(num1, num2)
+    }
+    else if (operator === '*') {
+        return multiply(num1, num2)
+    }
+    else if (operator === '/') {
+        return divide(num1, num2)
+    }
 }
