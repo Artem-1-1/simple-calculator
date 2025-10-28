fcf68dffe7cb8112b9d588f38ce8cd684dd96444
Artem-1-1 <aleksejchukartem@gmail.com>
2025-10-28 20:59:57 +0300

    Reworked appendNumber/Symbol function,Added EventLsitener to clear,delete and point button.Set basic textContent to zero.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let firstOperand = '';
 let secondOperand = '';
-let operator = null;
+let operator = '';
 
 const numberButtons = document.querySelectorAll('.number-btn');
 const operatorButtons = document.querySelectorAll('.symbol-btn');
@@ -9,14 +9,38 @@ const delButton = document.getElementById('deleteBtn');
 const pointButton = document.getElementById('pointBtn');
 const equalsButton = document.getElementById('equalsBtn');
 const screen = document.getElementById('currentScreen');
+screen.textContent = '0';
 
 numberButtons.forEach((button) =>
-  button.addEventListener('click', () => console.log(button.textContent))
+  button.addEventListener('click', () => appendNumber(button.textContent))
 )
 
 operatorButtons.forEach((button) =>
-  button.addEventListener('click', () => console.log(button.textContent))
+  button.addEventListener('click', () => appendSymbol(button.textContent.trim()))
 )
+
+clearButton.addEventListener('click', () => {
+    screen.textContent = '0';
+    firstOperand = '';
+    secondOperand = '';
+    operator = '';
+})
+
+delButton.addEventListener('click', () => {
+    if (screen.textContent.length > 1) {
+      screen.textContent = screen.textContent.slice(0, -1).trim()
+    }
+    else {
+      screen.textContent = '0';
+    }
+})
+
+pointButton.addEventListener('click', () => {
+    if (screen.textContent.at(-1) === '.') {
+        return;
+    }
+    screen.textContent += '.'
+})
 
 function add(num1, num2) {
     return num1 + num2;
@@ -53,3 +77,21 @@ function operate(operator, num1, num2) {
         return divide(num1, num2)
     }
 }
+
+function appendNumber(number) {
+    if (screen.textContent === '0') {   
+    screen.textContent = number;
+   } else {
+    screen.textContent += number;
+   }
+}
+
+function appendSymbol(symbol) {
+    if (screen.textContent.includes(symbol)) {
+      return;
+    } else {
+    screen.textContent += ` ${symbol} `;
+    operator = symbol;
+    }
+}
+
